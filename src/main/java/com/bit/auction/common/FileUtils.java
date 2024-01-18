30e7608381aef33dd77c90abd2d9bf7e3ea6dbe1
KimJongBeomm <whdqja0826@gmail.com>
2024-01-18 19:33:00 +0900

    20240118 앞단끝 js제외 뒷단 시작

--- src/main/java/com/bit/auction/common/FileUtils.java
+++ src/main/java/com/bit/auction/common/FileUtils.java
@@ -36,7 +36,7 @@ public class FileUtils {
     }
 
     public AuctionImgDTO parseFileInfo(MultipartFile multipartFile, String directory, String representativeImgName) {
-        String bucketName = "bitcamp-bucket-122";
+        String bucketName = "bitcamp-bucket-22";
 
         AuctionImgDTO auctionImgDTO = new AuctionImgDTO();
 
