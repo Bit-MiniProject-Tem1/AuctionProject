2281d92c54d6d0b14de3fe6ce34b98538a39367f
dmlgus0458 <dmlgus0458@naver.com>
2024-01-23 15:03:25 +0900

    .

---
+++ src/main/java/com/bit/auction/common/CkEditorImageUtils.java
@@ -0,0 +1,106 @@
+package com.bit.auction.common;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.bit.auction.configuration.NaverConfiguration;
+import com.bit.auction.goods.dto.DescriptionImgDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+@Component
+public class CkEditorImageUtils {
+    private final AmazonS3 s3;
+
+    @Value("${ncp.bucket}")
+    private String bucketName;
+
+    public CkEditorImageUtils(NaverConfiguration naverConfiguration) {
+        s3 = AmazonS3ClientBuilder.standard()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(
+                        naverConfiguration.getEndPoint(), naverConfiguration.getRegionName()
+                ))
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials(
+                                naverConfiguration.getAccessKey(), naverConfiguration.getSecretKey()
+                        )
+                ))
+                .build();
+    }
+
+    public DescriptionImgDTO parseFileInfo(MultipartFile multipartFile, String directory) {
+        DescriptionImgDTO descriptionImgDTO = new DescriptionImgDTO();
+
+        String auctionImgOrigin = multipartFile.getOriginalFilename();
+
+        SimpleDateFormat formater = new SimpleDateFormat("yyyyMMddHHmmsss");
+        Date nowDate = new Date();
+
+        String nowDateStr = formater.format(nowDate);
+
+        UUID uuid = UUID.randomUUID();
+
+        String imgName = nowDateStr + "_" + uuid.toString() + "_" + auctionImgOrigin;
+
+        String imgPath = directory + imgName;
+
+        try (InputStream fileIputStream = multipartFile.getInputStream()) {
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentType(multipartFile.getContentType());
+
+            PutObjectRequest putObjectRequest = new PutObjectRequest(
+                    bucketName,
+                    directory + imgName,
+                    fileIputStream,
+                    objectMetadata
+            ).withCannedAcl(CannedAccessControlList.PublicRead);
+
+            s3.putObject(putObjectRequest);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        descriptionImgDTO.setFileName(imgName);
+        descriptionImgDTO.setFileUrl("https://kr.object.ncloudstorage.com/bitcamp-bucket-122/" + imgPath);
+
+        return descriptionImgDTO;
+    }
+
+    public String parseFileInto(MultipartFile uploadFile) throws IOException {
+        SimpleDateFormat formater = new SimpleDateFormat("yyyyMMddHHmmsss");
+        Date nowDate = new Date();
+        String nowDateStr = formater.format(nowDate);
+
+        UUID uuid = UUID.randomUUID();
+        String originalFileName = uploadFile.getOriginalFilename();
+        String imgName = nowDateStr + "_" + uuid.toString() + "_" + originalFileName.substring(originalFileName.indexOf("."));
+
+
+        // String originalFileName = uploadFile.getOriginalFilename();
+        // String ext = originalFileName.substring(originalFileName.indexOf("."));
+        // String newFileName = UUID.randomUUID() + ext;
+        String realPath = System.getProperty("user.dir") + "\\src\\";
+
+        String savePath = realPath + "/main/resources/static/upload/" + imgName;
+        String uploadPath = realPath + "\\main\\resources\\static\\upload\\" + imgName;
+
+        Path path = Paths.get(savePath).toAbsolutePath();
+        uploadFile.transferTo(path.toFile());
+
+        return uploadPath;
+    }
+}
