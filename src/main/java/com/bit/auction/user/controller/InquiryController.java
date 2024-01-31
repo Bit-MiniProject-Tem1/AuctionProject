5417222827786129318f3ecc11add2a049197ad0
KimJongBeomm <whdqja0826@gmail.com>
2024-01-31 17:17:29 +0900

    20240131 프로필수정 완료 현재 문의하기,문의내역조회, 문의내역상세조회 하는중

--- src/main/java/com/bit/auction/user/controller/InquiryController.java
+++ src/main/java/com/bit/auction/user/controller/InquiryController.java
@@ -8,11 +8,16 @@ import com.bit.auction.user.dto.UserDTO;
 import com.bit.auction.user.entity.CustomUserDetails;
 import com.bit.auction.user.entity.Inquiry;
 import com.bit.auction.user.service.InquiryService;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -22,6 +27,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -29,7 +35,7 @@ import java.util.Map;
 
 
 @Slf4j
-@RequestMapping("/user")
+@RequestMapping("/inquiry")
 @RequiredArgsConstructor
 @Controller
 public class InquiryController {
@@ -41,12 +47,20 @@ public class InquiryController {
 
     private final Logger logger = LoggerFactory.getLogger(InquiryController.class);
 
-    @GetMapping("/inquiry-view")
-    public ModelAndView getInquiry() {
-
+    @GetMapping("/inquiry-list")
+    public ModelAndView getInquiryList(@PageableDefault(page = 0, size = 10) Pageable pageable,
+                                     InquiryDTO inquiryDTO) {
         ModelAndView mav = new ModelAndView();
 
-        mav.setViewName("user/mypage/insertInquiry.html");
+        logger.info("searchCondition: {}", inquiryDTO.getSearchCondition());
+        logger.info("searchKeyword: {}", inquiryDTO.getSearchKeyword());
+        logger.info("inquiryList: {}", inquiryService.getInquiryList(pageable, inquiryDTO));
+
+        mav.addObject("inquiryList", inquiryService.getInquiryList(pageable, inquiryDTO));
+        mav.addObject("searchCondition", inquiryDTO.getSearchCondition() == null ? "all" : inquiryDTO.getSearchCondition());
+        mav.addObject("searchKeyword", inquiryDTO.getSearchKeyword() == null ? "" : inquiryDTO.getSearchKeyword());
+
+        mav.setViewName("user/mypage/getInquiryList.html");
 
         return mav;
     }
@@ -63,6 +77,24 @@ public class InquiryController {
 
         return mav;
     }
+
+    @GetMapping("/insert-inquiry-view")
+    public ModelAndView insertInquiryView(@AuthenticationPrincipal CustomUserDetails customUserDetails) {
+
+        ModelAndView mav = new ModelAndView();
+
+        UserDTO loginUser = customUserDetails.getUser().toDTO();
+
+        if (loginUser == null) {
+            mav.setViewName("user/login/login.html");
+        } else {
+            mav.setViewName("user/mypage/insertInquiry.html");
+        }
+
+        return mav;
+    }
+
+
 
     @PostMapping("/inquiry")
     public ResponseEntity<?> insertInquiry(InquiryDTO inquiryDTO,
@@ -112,20 +144,142 @@ public class InquiryController {
 
     }
 
-    @GetMapping("/myInquiry-view")
-    public ModelAndView getMyInquiry(Model model) {
-        List<InquiryDTO> inquiryDTOList = inquiryService.findAll();
+    @PutMapping("/inquiry")
+    public ResponseEntity<?> updateInquiry(InquiryDTO inquiryDTO,
+                                         MultipartFile[] uploadFiles,
+                                         MultipartFile[] changeFiles,
+                                         @RequestParam("originFiles") String originFiles) {
+        ResponseDTO<Map<String, String>> response = new ResponseDTO<>();
 
-        model.addAttribute("inquiryList", inquiryDTOList);
+        try {
+            List<InquiryFileDTO> originFileList = new ObjectMapper().readValue(originFiles,
+                    new TypeReference<List<InquiryFileDTO>>() {});
 
-        ModelAndView mav = new ModelAndView();
+            List<InquiryFileDTO> uFileList = new ArrayList<>();
 
-        mav.setViewName("user/mypage/myinquiry.html");
+            for(int i = 0; i < originFileList.size(); i++) {
+                if(originFileList.get(i).getInquiryFileStatus().equals("U")) {
+                    for(int j = 0; j < changeFiles.length; j++) {
+                        if(originFileList.get(i).getNewFileName().equals(
+                                changeFiles[j].getOriginalFilename())) {
+                            InquiryFileDTO updateinquiryFile = fileUtils.parseFileInfo(changeFiles[j], "inquiry/");
 
-        return mav;
+                            updateinquiryFile.setInquiryNo(inquiryDTO.getInquiryNo());
+                            updateinquiryFile.setInquiryFileNo(originFileList.get(i).getInquiryFileNo());
+                            updateinquiryFile.setInquiryFileStatus("U");
+
+                            uFileList.add(updateinquiryFile);
+                        }
+                    }
+                } else if(originFileList.get(i).getInquiryFileStatus().equals("D")) {
+                    InquiryFileDTO deleteinquiryFile = new InquiryFileDTO();
+
+                    deleteinquiryFile.setInquiryNo(inquiryDTO.getInquiryNo());
+                    deleteinquiryFile.setInquiryFileNo(originFileList.get(i).getInquiryFileNo());
+                    deleteinquiryFile.setInquiryFileStatus("D");
+
+                    uFileList.add(deleteinquiryFile);
+                }
+            }
+
+            if(uploadFiles.length > 0) {
+                for(int i = 0; i < uploadFiles.length; i++) {
+                    if(!uploadFiles[i].getOriginalFilename().equals("") &&
+                            uploadFiles[i].getOriginalFilename() != null) {
+                        InquiryFileDTO insertinquiryFile = fileUtils.parseFileInfo(uploadFiles[i], "inquiry/");
+
+                        insertinquiryFile.setInquiryNo(inquiryDTO.getInquiryNo());
+                        insertinquiryFile.setInquiryFileStatus("I");
+
+                        uFileList.add(insertinquiryFile);
+                    }
+                }
+            }
+
+            inquiryService.updateInquiry(inquiryDTO, uFileList);
+
+            Map<String, String> returnMap = new HashMap<>();
+
+            returnMap.put("msg", "정상적으로 수정되었습니다.");
+
+            response.setItem(returnMap);
+            response.setStatusCode(HttpStatus.OK.value());
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            response.setErrorCode(701);
+            response.setErrorMessage(e.getMessage());
+            response.setStatusCode(HttpStatus.BAD_REQUEST.value());
+
+            return ResponseEntity.badRequest().body(response);
+        }
     }
 
+    @DeleteMapping("/inquiry/{inquiryNo}")
+    public ResponseEntity<?> deleteInquiry(@PathVariable("inquiryNo") Long inquiryNo) {
+        ResponseDTO<Map<String, String>> response = new ResponseDTO<>();
 
+        try {
+            inquiryService.deleteInquiry(inquiryNo);
 
+            Map<String, String> returnMap = new HashMap<>();
 
+            returnMap.put("msg", "정상적으로 삭제되었습니다.");
+
+            response.setItem(returnMap);
+            response.setStatusCode(HttpStatus.OK.value());
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+            response.setErrorCode(801);
+            response.setErrorMessage(e.getMessage());
+            response.setStatusCode(HttpStatus.BAD_REQUEST.value());
+
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    @GetMapping("/inquiry-cnt/{inquiryNo}")
+    public void updateInquiryCnt(@PathVariable("inquiryNo") Long inquiryNo,
+                               HttpServletResponse response) {
+        try {
+            inquiryService.updateInquiryCnt(inquiryNo);
+
+            response.sendRedirect("/inquiry/inquiry/" + inquiryNo);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @PostMapping("/save-inquiry-list")
+    public ResponseEntity<?> saveInquiryList(@RequestParam("changeRows") String changeRows) {
+        ResponseDTO<Map<String, String>> response = new ResponseDTO<>();
+
+        try {
+            List<Map<String, String>> changeRowsList = new ObjectMapper().readValue(changeRows,
+                    new TypeReference<List<Map<String, String>>>() {});
+
+            inquiryService.saveInquiryList(changeRowsList);
+
+            Map<String, String> returnMap = new HashMap<>();
+
+            returnMap.put("msg", "정상적으로 저장되었습니다.");
+
+            response.setItem(returnMap);
+            response.setStatusCode(HttpStatus.OK.value());
+
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+
+            response.setErrorCode(801);
+            response.setErrorMessage(e.getMessage());
+            response.setStatusCode(HttpStatus.BAD_REQUEST.value());
+
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
 }
--- src/main/java/com/bit/auction/user/service/InquiryService.java
+++ src/main/java/com/bit/auction/user/service/InquiryService.java
@@ -1,9 +1,14 @@
 package com.bit.auction.user.service;
 
 import com.bit.auction.user.dto.InquiryDTO;
+import com.bit.auction.user.dto.InquiryFileDTO;
 import com.bit.auction.user.entity.Inquiry;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public interface InquiryService {
 
@@ -11,5 +16,27 @@ public interface InquiryService {
 
     InquiryDTO getInquiry(Long inquiryNo);
 
-    List<InquiryDTO> findAll();
+    void updateInquiry(InquiryDTO inquiryDTO, List<InquiryFileDTO> uFileList);
+
+    void deleteInquiry(Long inquiryNo);
+
+    Page<InquiryDTO> getInquiryList(Pageable pageable, InquiryDTO inquiryDTO);
+
+    Page<InquiryDTO> findAll(Pageable pageable);
+
+    void save(InquiryDTO inquiryDTO);
+
+    InquiryDTO findById(Long inquiryNo);
+
+    InquiryDTO findByInquiryTitle(String inquiryTitle);
+
+    InquiryDTO findByInquiryTitleAndInquiryContent(String InquiryTitle, String InquiryContent);
+
+    List<InquiryDTO> findByInquiryTitleContaining(String searchKeyword);
+
+    List<InquiryDTO> search(String searchKeyword, String searchCondition);
+
+    void updateInquiryCnt(Long inquiryNo);
+
+    void saveInquiryList(List<Map<String, String>> changeRowsList);
 }
